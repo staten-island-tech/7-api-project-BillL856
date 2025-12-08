d6e9dc1a55f10cd1f962b110f92820aa40686b58
BillL856 <billl14@nycstudents.net>
2025-12-08 08:44:52 -0500

    plmkoijnb

--- realapi.py
+++ realapi.py
@@ -19,21 +19,24 @@ print(pokemon) """
 
 import requests
 
-e1=input("What measurement system are you using. Ex. Time")
-e2=input("What unit are you converting from. Ex. Second")
-e3=input("What unit are you converting to. Ex. Day")
-e4=input("How many units do you want to convert? Ex. 8748989638")
-def units(type,unit,unit2,quantity):
+constantinople=True
+while constantinople==True:
+
+    e1=input("What measurement system are you using. Ex. Time")
+    e2=input("What unit are you converting from. Ex. Second")
+    e3=input("What unit are you converting to. Ex. Day")
+    e4=input("How many units do you want to convert? Ex. 8748989638")
+    def units(type,unit,unit2,quantity):
     
-    response = requests.get(f"https://api.unusualunits.com/convert/{type.lower()}/{unit.lower()}/{unit2.lower()}/{quantity}")
-    if response.status_code !=200:
-        print("Error")
-        return None
+        response = requests.get(f"https://api.unusualunits.com/convert/{type.lower()}/{unit.lower()}/{unit2.lower()}/{quantity}")
+        if response.status_code !=200:
+            print("Error")
+            return None
     
-    data=response.json()
-    return {
+        data=response.json()
+        return {
         "Value":data["result"]
-    }
-horastodias=units(e1,e2,e3,e4)
-print(horastodias)
-    
+        }
+
+    horastodias=units(e1,e2,e3,e4)
+    print(horastodias)
